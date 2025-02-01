69f3170dc0ec844d0ff285b9d42b429b379eedd5
Brandon Benefield <bsquared18@gmail.com>
2025-01-31 21:28:34 -0500

    Add Tailwind CSS setup and integrate NativeWind for styling

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,8 +5,11 @@ import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { ActivityIndicator, MD2Colors } from 'react-native-paper';
+import { verifyInstallation } from 'nativewind';
 
 export default function HomeScreen() {
+  verifyInstallation()
+  
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,17 +4,18 @@ import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
+import { PaperProvider } from 'react-native-paper';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
-import { MD3DarkTheme, MD3Theme, PaperProvider } from 'react-native-paper';
 import LLDarkTheme from '@/assets/themes/LLDarkTheme';
+import "@/assets/css/global.css"
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  // const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
